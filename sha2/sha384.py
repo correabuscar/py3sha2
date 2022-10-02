dac071ddf15eb6c50a1912d4109c8f2d2d9b8d8f
correabuscar <correabuscar+githubcommits@gmail.com>
2022-10-02 22:12:01 +0200

    forgot to write a file
    commented out computed stuff in base class

--- sha2/sha384.py
+++ sha2/sha384.py
@@ -11,5 +11,6 @@ class sha384(sha512):
     _h = (0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939,
           0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4)
     _output_size = 6
-    digest_size = 48
+    #digest_size = 48
+    #element_size_bytes=8
 
